802fd616cf5e4d20f6720c94a325b7c0e53888c9
nyx-desdev <nyx.desdev@gmail.com>
2023-02-12 14:49:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import AwesomeTestimonial from "react-awesome-testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AwesomeTestimonial
+      testimonials={[
+        {
+          name: "Eva",
+          company: "Amazon",
+          img_src: "https://i.ibb.co/84h8svL/eight.png",
+          review:
+            "Lorem 1 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Evelyn",
+          company: "Netflix",
+          img_src: "https://i.ibb.co/k8Jnx61/five.png",
+          review:
+            "Lorem 2 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Jack",
+          company: "Google",
+          img_src: "https://i.ibb.co/Yj8pMF8/four.png",
+          review:
+            "Lorem 3 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Sam",
+          company: "Microsoft",
+          img_src: "https://i.ibb.co/ph360c6/nine.png",
+          review:
+            "Lorem 4 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Abigail",
+          company: "Netflix",
+          img_src: "https://i.ibb.co/gwNmrLn/one.png",
+          review:
+            "Lorem 5 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Mortal",
+          company: "Google",
+          img_src: "https://i.ibb.co/6PF0kMg/seven.png",
+          review:
+            "Lorem 6 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Bruno",
+          company: "Netflix",
+          img_src: "https://i.ibb.co/7G9rtfD/six.png",
+          review:
+            "Lorem 7 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Vernoica",
+          company: "Facebook",
+          img_src: "https://i.ibb.co/pXMvXhK/three.png",
+          review:
+            "Lorem 8 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+        {
+          name: "Astro",
+          company: "Netflix",
+          img_src: "https://i.ibb.co/WzJD5sj/two.png",
+          review:
+            "Lorem 9 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+        },
+      ]}
+    />
   );
 }
 
